a10c5928c5d6471856619bdd330f1ff331e02924
DADU-Y <daduyameen2@gmail.com>
2025-06-24 15:26:49 +0500

    Create Assiement of React

---
+++ Assiement no 1/src/main.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const divElement = document.getElementById("root");
+
+ReactDOM.createRoot(divElement).render(
+  <h1 style={{textAlign:"center",background:"cyan",color:"brown"}}>Hello World And Hello react</h1>
+)
